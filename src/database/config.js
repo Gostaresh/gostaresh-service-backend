19494d13c314ea31721a0e2e07d1c73f145f479b
Ehsan Tabatabaei <ehsanei77@gmail.com>
2025-12-29 16:38:15 +0330

    `Update .env and database/config.js to include production database settings`

--- src/database/config.js
+++ src/database/config.js
@@ -1,33 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const common = {
-  dialect: 'mysql',
+  dialect: "mysql",
   logging: false,
 };
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
+    username: process.env.DB_USER || "root",
     password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'gostaresh_dev',
-    host: process.env.DB_HOST || '127.0.0.1',
+    database: process.env.DB_NAME || "gostaresh_dev",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     ...common,
   },
   test: {
-    username: process.env.DB_USER || 'root',
+    username: process.env.DB_USER || "root",
     password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME_TEST || 'gostaresh_test',
-    host: process.env.DB_HOST || '127.0.0.1',
+    database: process.env.DB_NAME_TEST || "gostaresh_test",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     ...common,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASS_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD || "127.0.0.1",
+    port: process.env.DB_PORT_PROD ? Number(process.env.DB_PORT_PROD) : 3306,
     ...common,
   },
 };
